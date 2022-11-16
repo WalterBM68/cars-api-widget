e122b0d1863a4511ead5bf1e7438dc9c75b7f4f7
WalterBM68 <Wbmanjanja@gmail.com>
2022-11-16 10:27:36 +0200

    widget has been fixed

--- index.js
+++ index.js
@@ -7,26 +7,54 @@ const theColorTemplate = Handlebars.compile(colorTemplateText.innerText);
 //For the brand
 const brandTemplateText = document.querySelector('.brandTemplate');
 const theBrandTemplate = Handlebars.compile(brandTemplateText.innerText);
+//Filter cars by brand
+const filterByBrandTemplateText = document.querySelector('.filterByBrandTemplate');
+const filterTemplate = Handlebars.compile(filterByBrandTemplateText.innerText);
+//Filter cars by color
+const filterByColorTemplateText = document.querySelector('.filterByColorTemplate');
+const filterByColorTemplate = Handlebars.compile(filterByColorTemplateText.innerText);
 
 //Element for cars
 const carsElem = document.querySelector('.cars');
+const filterCarsElem = document.querySelector('.filter');
 //Element for color
 const colorElem = document.querySelector('.colors');
 //Element for brand
 const brandElem = document.querySelector('.brand');
 //Filtering Element
-const filterElem = document.querySelector('.filter');
-
+const theCarsElem = document.querySelector('.theCars');
+// select cars Element
+const selectCars = document.querySelector('.selectCars');
+//filtering buttons
+const filterBtn = document.querySelector('.filter');
 //Filtering
-carsElem.addEventListener('click', function(evt){
 
+filterBtn.addEventListener('click', function(){
+    
+    axios
+    .get(`https://api-tutor.herokuapp.com/v1/cars/make/${selectCars.value}`)
+    .then(result => {
+        const theCarBrands = result.data;
+        theCarsElem.innerHTML = filterTemplate({
+            theCarBrands
+        });
+    });
+
+    axios
+    .get(`https://api-tutor.herokuapp.com/v1/cars/color/${selectCars.value}`)
+    .then(result => {
+        const theCarColor = result.data;
+        theCarsElem.innerHTML = filterByColorTemplate({
+            theCarColor
+        });
+    });    
+    
 });
 
 axios
 .get("https://api-tutor.herokuapp.com/v1/cars")
 .then(result => {
     const cars = result.data;
-
     carsElem.innerHTML = theCarsTemplate({
         cars
     });
@@ -36,7 +64,6 @@ axios
 .get("https://api-tutor.herokuapp.com/v1/colors") 
 .then(result => {
     const color = result.data;
-
     colorElem.innerHTML = theColorTemplate({
         color
     });
@@ -46,7 +73,6 @@ axios
 .get("https://api-tutor.herokuapp.com/v1/makes") 
 .then(result => {
     const brand = result.data;
-
     brandElem.innerHTML = theBrandTemplate({
         brand
     });
